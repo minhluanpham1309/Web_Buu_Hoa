ca2b306c30c85f7244c726b12b19db4cb18b098a
minhluanpham1309 <minhluanpham1309@gmail.com>
2018-05-13 15:58:31 +0700

    creat project

---
+++ Web_BuuHoa_Notary/Assets/Client/js/Custom_Client.js
@@ -0,0 +1,48 @@
+﻿// Slide show
+$(function () {
+    // Slideshow 4
+    $("#slider4").responsiveSlides({
+        auto: true,
+        pager: true,
+        nav: true,
+        speed: 500,
+        namespace: "callbacks",
+        before: function () {
+            $('.events').append("<li>before event fired.</li>");
+        },
+        after: function () {
+            $('.events').append("<li>after event fired.</li>");
+        }
+    });
+
+    $("#slider3").responsiveSlides({
+        auto: true,
+        pager: false,
+        nav: false,
+        speed: 500,
+        namespace: "callbacks",
+        before: function () {
+            $('.events').append("<li>before event fired.</li>");
+        },
+        after: function () {
+            $('.events').append("<li>after event fired.</li>");
+        }
+    });
+
+    $(".scroll").click(function (event) {
+        event.preventDefault();
+        $('html,body').animate({
+            scrollTop: $(this.hash).offset().top
+        }, 1000);
+    });
+
+    $().UItoTop({
+        easingType: 'easeOutQuart'
+    });
+});
+
+$('.jarallax').jarallax({
+    speed: 0.5,
+    imgWidth: 1366,
+    imgHeight: 768
+})
---
+++ Web_BuuHoa_Notary/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Web_BuuHoa_Notary.MvcApplication" Language="C#" %>
